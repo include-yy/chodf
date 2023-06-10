cb004b3fa6fb1c513dbbf6d9580c92303f7724ec
include-yy <969041171@qq.com>
2023-06-11 00:09:20 +0900

    improve source and test

--- chodf.el
+++ chodf.el
@@ -26,13 +26,11 @@
 ;; [https://github.com/cho45/jsdeferred]
 ;; [https://github.com/kiwanami/emacs-deferred]
 
-;; The API is almost the same as JSDeferred written by cho45. See the
-;; JSDeferred and Mochikit.Async web sites for further documentations.
+;; The API is almost the same as JSDeferred written by cho45.
 ;; [https://github.com/cho45/jsdeferred]
-;; [http://mochikit.com/doc/html/MochiKit/Async.html]
 
 ;; A good introduction document (JavaScript)
-;; [http://cho45.stfuawsc.com/jsdeferred/doc/intro.en.html]
+;; [https://cho45.stfuawsc.com/jsdeferred/doc/intro.en.html]
 ;; [https://cho45.stfuawsc.com/jsdeferred/]
 
 (defun chodf-ok (x)
@@ -53,7 +51,7 @@
   (n nil :documentation "points to the next deferred object or nil"))
 
 (defun chodf--post (odf okng fun)
-  "[internal] create a deferred object, make OBF points to it.
+  "[internal] Creates a deferred object, make OBF points to it.
 if OKNG is `:ok', then obj's OKCB is FUN, otherwise NGCB."
   (let ((new (chodf-new)))
     (pcase okng
@@ -63,17 +61,17 @@ if OKNG is `:ok', then obj's OKCB is FUN, otherwise NGCB."
     new))
 
 (defun chodf-next (odf fun)
-  "Create new deferred and sets FUN as its ok callback, \
+  "Creates new deferred and sets FUN as its callback, \
 then connect ODF to it"
   (chodf--post odf :ok fun))
 (defun chodf-error (odf fun)
-  "Create new deferred and sets FUN as its err callback, then connect ODF to it.
+  "Creates new deferred and sets FUN as its errback, then connect ODF to it.
 if FUN does not signal an error but just returns normal value,
 deferred treats the give error is recovery and continue chain"
   (chodf--post odf :ng fun))
 (defun chodf-ner (odf okfn ngfn)
-  "Create new deferred and sets okcb to OKFN, ngcb to NGFN, then connect ODF to it.
-this function doens't exist in jsdeferred, I add it just for fun :p"
+  "Creates new deferred and sets okcb to OKFN, ngcb to NGFN, then connect ODF to it.
+this function doens't exist in jsdeferred, I add it just for code simplification."
   (let ((new (chodf-new)))
     (setf (chodf-okcb new) okfn)
     (setf (chodf-ngcb new) ngfn)
@@ -83,14 +81,14 @@ this function doens't exist in jsdeferred, I add it just for fun :p"
 (defun chodf--fire (odf okng value)
   "[internal] Executing deferred callback chosen by OKNG.
 OKNG can either be `:ok' or `:ng'. VALUE is arg for callback.
-If an error is signaled by ok or ng function, and deferred's `n' exists, then\
+If an error is signaled by ok or ng function, and deferred's `n' exists, then \
 the next deferred object's ngcb function will be called"
+  (cl-assert (member okng '(:ok :ng)))
   (let ((next :ok))
     (condition-case err
 	(pcase okng
 	  (:ok (setq value (funcall (chodf-okcb odf) value)))
-	  (:ng (setq value (funcall (chodf-ngcb odf) value)))
-	  (_ (error "chodf--fire ~ unknown choice: `%s'" okng)))
+	  (:ng (setq value (funcall (chodf-ngcb odf) value))))
       (error
        (setq next :ng)
        (setq value err)))
@@ -101,7 +99,8 @@ the next deferred object's ngcb function will be called"
     odf))
 
 (defun chodf-call (odf val)
-  "Invokes self callback chain."
+  "Invokes self callback chain.
+Used this in async function's callback to start a deferred object (chain)."
   (chodf--fire odf :ok val))
 (defun chodf-fail (odf val)
   "Invokes self errorback chain.
@@ -109,7 +108,7 @@ Use this function for explicit errors. (eg. HTTP request failed)"
   (chodf--fire odf :ng val))
 
 (defun chodf-sync! (d)
-  "Wait for the given deferred task. For test and debugging.
+  "Waits for the given deferred task. For test and debugging.
 Error is raised if it is not processed within deferred chain D."
   (let* ((random-symbol (gensym))
 	 (last-value random-symbol)
@@ -125,27 +124,44 @@ Error is raised if it is not processed within deferred chain D."
       (chodf-ng uncaught-error))
     last-value))
 
-(defun chodf-nextx (&optional fun)
-  "Shorthand for creating new deferred which is called after current task"
+(defun chodf-nextx (&optional fun wait-time)
+  "Shorthand for creating new deferred which is called after current task.
+If WAIT-TIME is specified, deferred will start after WAIT-TIME seconds, \
+otherwise it is ZERO."
+  (setq wait-time (or wait-time 0))
   (let ((d (chodf-new)))
     (when fun (setf (chodf-okcb d) fun))
-    (run-at-time 0 nil
+    (run-at-time wait-time nil
 		 (lambda () (chodf-call d nil)))
     d))
 
 (defun chodf-callx (fun &rest args)
-  "Calls function asynchronous. A shorthand for `chodf-nextx'"
+  "Calls function asynchronously. A shorthand for `chodf-nextx'"
   (chodf-nextx (lambda (_)
 		 (apply fun args))))
 
 (defun chodf-wait (n)
-  "Wait returns deferred that will be called after N seconds elapsed"
+  "Waits and return deferred object that will be called after N seconds elapsed"
   (let ((d (chodf-new))
 	(ti (float-time)))
     (run-at-time n nil
 		 (lambda ()
 		   (chodf-call d (- (float-time) ti))))
     d))
+
+(defun chodf-connect (fun &optional testfn errtype)
+  "*Deferredlizes* a function.
+the original function's last arg must be a callback"
+  (setq testfn (or testfn (lambda (_) t)))
+  (setq errtype (or errtype 'error))
+  (lambda (&rest args)
+    (let ((d (chodf-new)))
+      (let ((callback (lambda (x)
+			(if (funcall testfn x)
+			    (chodf-call d x)
+			  (chodf-fail d `(,errtype . ,x))))))
+	(apply fun `(,@args ,callback))
+	d))))
 
 (defun chodf-parallel (&rest fun-or-d-ls)
   "Parallel wraps up deferredlist to one deferred.
@@ -173,9 +189,9 @@ This is useful when some asynchronous resources are required."
     ret))
 
 (defun chodf-chain (&rest funs-or-err)
-  "Construct deferred chain and return its deferred.
+  "Constructs deferred chain and return its deferred.
 Shorthand for construct deferred chains.
-If function is after a :err symbol, then it will be errback, otherwise callback"
+If function is after a `:err' symbol, then it will be an errback, otherwise a callback"
   (let ((chain (chodf-nextx))
 	(flag nil))
     (mapc (lambda (obj)
@@ -191,8 +207,9 @@ If function is after a :err symbol, then it will be errback, otherwise callback"
 
 (defun chodf-earlier (&rest chodfs)
   "Continue process when one deferred in deferredlist has completed. \
-Others will be canceled. parallel ('and' processing) <=> earlier ('or' processing)
-return value is cons (index . value), index is the earlist deferred's order in arglist"
+Others will be canceled.
+return value is cons (index . value), index is the earlist deferred's order in arglist.
+parallel ('and' processing) <=> earlier ('or' processing)."
   (let* ((ret (chodf-new))
 	 (num 0)
 	 (flag t))
@@ -200,18 +217,25 @@ return value is cons (index . value), index is the earlist deferred's order in a
      (lambda (d i)
        (chodf-ner
 	d (lambda (v)
-	    (when flag
-	      (setq flag nil)
-	      (chodf-call ret (cons i v))))
+	    (when flag (setq flag nil)
+		  (chodf-call ret (cons i v))))
 	(lambda (e)
-	  (when flag
-	    (setq flag nil)
-	    (chodf-fail ret e))))
+	  (when flag (setq flag nil)
+		(chodf-fail ret e))))
        (cl-incf num))
      chodfs)
-    (when (= num 0)
-      (chodf-nextx (lambda (_) (chodf-call ret nil))))
+    (when (= num 0) (chodf-nextx
+		     (lambda (_)
+		       (chodf-call ret nil))))
     ret))
+
+(defvar chodf-ticktime 0.001
+  "Waiting time between asynchronous tasks (second).
+The shorter waiting time increases the load of Emacs. The end
+user can tune this parameter. However, applications should not
+modify it because the applications run on various environments.
+
+Used only for `chodf-loop', `chodf-repeat' and `chodf-retry'")
 
 (defun chodf-loop (n fun)
   "Provides non-blocking loop.
@@ -225,50 +249,50 @@ This function waits a deferred returned by loop function."
 		     (chodf-next
 		      ret (lambda (r)
 			    (setq ret r)
-			    (chodf-nextx it)))
-		   (chodf-nextx it)))))
+			    (chodf-nextx it chodf-ticktime)))
+		   (chodf-nextx it chodf-ticktime)))))
     (when (> n 0) (chodf-nextx it))))
 
+(defvar chodf-repeat-maxtime 0.01
+  "Used for `chodf-repeat', if loop spends over it, start the next task.
+Default value is 0.01, which means 10ms.")
+
 (defun chodf-repeat (n fun)
-  "* Loop `n` times with `fun`.
-* This function automatically returns UI-control to browser, if the loop spends over 20msec.
-* This is useful for huge loop not to block browser UI.
-* This function can't wait a deferred returned by loop function, compared with Deferred.loop."
+  "Loop `n` times with `fun`.
+This function automatically returns UI-control to emacs \
+if the loop spends over `chodf-repeat-maxtime'.
+This is useful for huge loop not to block emacs UI.
+This function can't wait a deferred returned by loop function, compared with chodf-loop."
   (let ((i 0) it)
-    (chodf-nextx
-     (setq it (lambda (_)
-		(let ((ti (float-time)))
-		  (while (and (< i n)
-			      (< (- (float-time) ti) 0.02)) ; 20ms
-		    (cl-incf i)
-		    (funcall fun))
-		  (when (< i n) (chodf-callx it nil))))))))
+    (when (> n 0)
+      (chodf-nextx
+       (setq it (lambda (_)
+		  (let ((ti (float-time)))
+		    (while (and (< i n)
+				(< (- (float-time) ti) chodf-repeat-maxtime))
+		      (cl-incf i)
+		      (funcall fun))
+		    (when (< i n) (chodf-nextx it chodf-ticktime)))))))))
 
 (defun chodf-retry (cnt fun &optional wait)
-  "Try func (returns Deferred) till it finish without exceptions."
-  (setq wait (or wait 0))
-  (let ((d (chodf-new)) retry)
+  "Try func (returns Deferred) till it finish without exceptions.
+If specified, WAIT is waiting time between each try, otherwise `chodf-ticktime'"
+  (setq wait (or wait chodf-ticktime))
+  (let ((d (chodf-new))
+	retry)
     (setq retry
 	  (lambda ()
 	    (let ((m (funcall fun)))
-	      (setq m (chodf-ner
-		       m
-		       (lambda (mes) (chodf-call d mes))
-		       (lambda (e)
-			 (if (<= (cl-decf cnt) 0)
-			     (chodf-fail e)
-			   (run-at-time wait nil retry))))))))
-    (run-at-time 0 nil retry)
-    d))
+	      (chodf-ner
+	       m (lambda (res)
+		   (chodf-call d res))
+	       (lambda (e)
+		 (if (<= (cl-decf cnt) 0)
+		     (chodf-fail d e)
+		   (run-at-time wait nil retry)))))))
+    (when (> cnt 0)
+      (run-at-time 0 nil retry)
+      d)))
 
-(defun chodf-connect (fun &optional testfn)
-  (setq testfn (or testfn (lambda (_) t)))
-  (let ((d (chodf-new)))
-    (let ((callback (lambda (x)
-		      (if (funcall testfn x)
-			  (chodf-call d x)
-			(chodf-fail d x)))))
-      (lambda (&rest args)
-	(apply fun `(,@args ,callback))))))
-
+(provide 'chodf)
 ;;; chodf.el ends here
